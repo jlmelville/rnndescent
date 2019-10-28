b1fae311a89ee119cc52afed466b7151d5c86f72
James Melville <jlmelville@gmail.com>
2019-10-27 19:08:05 -0700

    Add test for brute force.

---
+++ tests/testthat/test_brute_force.R
@@ -0,0 +1,12 @@
+library(rnndescent)
+context("Brute force")
+
+set.seed(1337)
+rnbrs <- nn_brute_force(ui10, k = 4, n_threads = 0)
+check_nbrs_idx(rnbrs$idx)
+check_nbrs_dist(rnbrs, ui10_eucd, tol = 1e-6)
+
+set.seed(1337)
+rnbrs <- nn_brute_force(ui10, k = 4, n_threads = 1)
+check_nbrs_idx(rnbrs$idx)
+check_nbrs_dist(rnbrs, ui10_eucd, tol = 1e-6)
