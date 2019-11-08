9b7ba37aafc19fe6fb84304ff0e46fc06bac5b83
James Melville <jlmelville@gmail.com>
2019-11-08 10:13:33 -0800

    Tests for brute force query.

--- tests/testthat/helper_nncheck.R
+++ tests/testthat/helper_nncheck.R
@@ -1,12 +1,19 @@
-are_valid_neighbors <- function(nnidx, i, nr, nc) {
-  i != nnidx[i, 2:nc] & nnidx[i, 2:nc] > 0 & nnidx[i, 2:nc] <= nr
+
+# Graph Construction Helpers ----------------------------------------------
+
+# i should not appear in the "true" neighbors (i.e. it should be the first
+# neighbor), indexes should be in the range (1, nr) and indexes should be unique
+are_valid_neighbors <- function(nnidx, i) {
+  nr <- nrow(nnidx)
+  nc <- ncol(nnidx)
+  true_nbrs <- nnidx[i, 2:nc]
+  all(i != true_nbrs & true_nbrs > 0 & true_nbrs <= nr) && length(unique(true_nbrs)) == nc - 1
 }
 
 check_nbrs_idx <- function(nnidx) {
   nr <- nrow(nnidx)
-  nc <- ncol(nnidx)
   for (i in 1:nr) {
-    testthat::expect_true(all(are_valid_neighbors(nnidx, i, nr, nc), label = i))
+    testthat::expect_true(all(are_valid_neighbors(nnidx, i), label = i))
   }
 }
 
@@ -20,4 +27,52 @@ check_nbrs_dist <- function(nn, expected_dist, tol = .Machine$double.eps) {
       )
     }
   }
+}
+
+check_nbrs <- function(nn, expected_dist, tol = .Machine$double.eps) {
+  check_nbrs_idx(nn$idx)
+  check_nbrs_dist(nn, expected_dist, tol = tol)
+}
+
+# Query Helpers -----------------------------------------------------------
+
+# indexes should be in the range (1, nref) and indexes should be unique
+are_valid_query_neighbors <- function(nnidx, i, nref) {
+  nc <- ncol(nnidx)
+  query_nbrs <- nnidx[i, ]
+  all(query_nbrs > 0 & query_nbrs <= nref) && length(unique(query_nbrs)) == nc
+}
+
+
+check_query_nbrs_idx <- function(nnidx, nref) {
+  nr <- nrow(nnidx)
+  for (i in 1:nr) {
+    testthat::expect_true(all(are_valid_query_neighbors(nnidx, i, nref), label = i))
+  }
+}
+
+check_query_nbrs_dist <- function(nn, expected_dist, ref_range, query_range, tol = .Machine$double.eps) {
+  n_queries <- nrow(nn$idx)
+  n_nbrs <- ncol(nn$idx)
+  for (i in 1:n_queries) {
+    for (j in 1:n_nbrs) {
+      testthat::expect_equal(nn$dist[i, j],
+                             expected_dist[query_range[i], ref_range[nn$idx[i, j]]],
+                             tol = tol, label = paste0(i, ", ", j),
+      )
+    }
+  }
+}
+
+check_nn_matrix_dim <- function(m, query, k) {
+  expect_equal(nrow(m), nrow(query))
+  expect_equal(ncol(m), k)
+}
+
+check_query_nbrs <- function(nn, query, ref_range, query_range, k, expected_dist, tol = .Machine$double.eps) {
+  check_nn_matrix_dim(nn$idx, query, k)
+  check_nn_matrix_dim(nn$dist, query, k)
+  nref <- length(ref_range)
+  check_query_nbrs_idx(nn$idx, nref)
+  check_query_nbrs_dist(nn, expected_dist, ref_range, query_range, tol)
 }
--- tests/testthat/test_brute_force.R
+++ tests/testthat/test_brute_force.R
@@ -1,12 +1,20 @@
 library(rnndescent)
-context("Brute force")
+context("Brute force construction")
 
-set.seed(1337)
 rnbrs <- brute_force_knn(ui10, k = 4, n_threads = 0)
-check_nbrs_idx(rnbrs$idx)
-check_nbrs_dist(rnbrs, ui10_eucd, tol = 1e-6)
+check_nbrs(rnbrs, ui10_eucd, tol = 1e-6)
 
-set.seed(1337)
 rnbrs <- brute_force_knn(ui10, k = 4, n_threads = 1)
-check_nbrs_idx(rnbrs$idx)
-check_nbrs_dist(rnbrs, ui10_eucd, tol = 1e-6)
+check_nbrs(rnbrs, ui10_eucd, tol = 1e-6)
+
+# Queries -----------------------------------------------------------------
+
+context("Brute force queries")
+ui6 <- ui10[1:6, ]
+ui4 <- ui10[7:10, ]
+
+qnbrs4 <- brute_force_knn_query(reference = ui6, query = ui4, k = 4)
+check_query_nbrs(nn = qnbrs4, query = ui4, ref_range = 1:6, query_range = 7:10, k = 4, expected_dist = ui10_eucd, tol = 1e-6)
+
+qnbrs6 <- brute_force_knn_query(reference = ui4, query = ui6, k = 4)
+check_query_nbrs(nn = qnbrs6, query = ui6, ref_range = 7:10, query_range = 1:6, k = 4, expected_dist = ui10_eucd, tol = 1e-6)
