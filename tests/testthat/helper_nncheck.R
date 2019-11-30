79e5a4dd3e2257aa650be20bec4332deea1405cf
James Melville <jlmelville@gmail.com>
2019-11-30 01:08:32 -0800

    more code formatting.

--- tests/testthat/helper_nncheck.R
+++ tests/testthat/helper_nncheck.R
@@ -35,8 +35,9 @@ check_nbrs_dist <- function(nn, expected_dist, tol = .Machine$double.eps) {
 
 check_nbrs_order <- function(nn) {
   expect_true(all(apply(nn$dist, 1, order) ==
-                    matrix(rep(1:ncol(nn$idx), times = nrow(nn$idx)),
-                           nrow = ncol(nn$idx))))
+    matrix(rep(1:ncol(nn$idx), times = nrow(nn$idx)),
+      nrow = ncol(nn$idx)
+    )))
 }
 
 check_nbrs <- function(nn, expected_dist, tol = .Machine$double.eps, check_order = TRUE) {
