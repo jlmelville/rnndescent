fe6393d504157404edbd4a3740af39d30e0bf5bc
James Melville <jlmelville@gmail.com>
2020-10-24 09:26:33 -0700

    reformatting

--- tests/testthat/test_koccurrences.R
+++ tests/testthat/test_koccurrences.R
@@ -4,10 +4,14 @@ context("Reverse neighbors")
 rnbrs <- brute_force_knn(ui10, k = 4, n_threads = 0)
 hub10 <- k_occur(rnbrs$idx)
 expect_equal(hub10, c(3, 5, 7, 3, 5, 3, 5, 4, 2, 3))
-expect_equal(k_occur(rnbrs$idx, include_self = FALSE),
-             c(2, 4, 6, 2, 4, 2, 4, 3, 1, 2))
-expect_equal(k_occur(rnbrs$idx, k = 2),
-             c(2, 1, 2, 2, 2, 3, 3, 2, 2, 1))
+expect_equal(
+  k_occur(rnbrs$idx, include_self = FALSE),
+  c(2, 4, 6, 2, 4, 2, 4, 3, 1, 2)
+)
+expect_equal(
+  k_occur(rnbrs$idx, k = 2),
+  c(2, 1, 2, 2, 2, 3, 3, 2, 2, 1)
+)
 
 qnbrs4 <- brute_force_knn_query(reference = ui6, query = ui4, k = 4)
 expect_equal(k_occur(qnbrs4$idx), c(1, 3, 4, 3, 4, 1))
