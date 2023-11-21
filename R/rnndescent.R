a5b83ccb5a0e6cc6d8a724e32b831826823a8816
James Melville <jlmelville@gmail.com>
2023-11-20 16:21:24 -0800

    Pass progress option to nnd

--- R/rnndescent.R
+++ R/rnndescent.R
@@ -280,6 +280,7 @@ rnnd_build <- function(data,
     low_memory = low_memory,
     n_threads = n_threads,
     verbose = verbose,
+    progress = progress,
     obs = "C"
   )
 
@@ -713,6 +714,7 @@ rnnd_knn <- function(data,
     low_memory = low_memory,
     n_threads = n_threads,
     verbose = verbose,
+    progress = progress,
     obs = "C"
   )
 }
